e213ed7bdbc667a91f35018061517e3e3df27874
admin <admin@DESKTOP-D9F9OEG>
2021-12-10 17:09:08 +0530

    First commit....

---
+++ src/projectgit/pro_git.java
@@ -0,0 +1,19 @@
+package projectgit;
+
+public class pro_git {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something");
+		}
+		else
+		{
+			System.out.println("Do work");
+		}
+
+	}
+
+}
+
